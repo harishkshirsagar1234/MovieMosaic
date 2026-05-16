69b6efb69425e2f12e3a9b6958fee1aad42e5c96
Harish Kshirsagar <Harishkshirsagar123@gmail.com>
2026-05-16 15:32:40 +0530

    test

--- MovieMosaic/Views/MovieListView.swift
+++ MovieMosaic/Views/MovieListView.swift
@@ -17,7 +17,7 @@ struct MovieListView: View {
             VStack {
                 TextField("Enter Movie Name", text: $searchText)
                     .accessibilityIdentifier("SearchUsersField")
-                    .frame(height: 30)
+                    .frame(height: 40)
                     .textFieldStyle(.plain)
                     .background(.gray.opacity(0.5))
                     .cornerRadius(20)
